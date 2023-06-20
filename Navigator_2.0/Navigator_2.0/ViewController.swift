18252d6e82f2c712996228a217503b84447b282f
PoletaevEvgeny <poletaevev89@gmail.com>
2023-06-21 01:58:15 +0300

    feature/iosui-1.3 - create project

---
+++ Navigator_2.0/Navigator_2.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Navigator_2.0
+//
+//  Created by Евгений Полетаев on 21.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
