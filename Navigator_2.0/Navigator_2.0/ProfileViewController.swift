11c9a4b5858100a14e7624b1b3bffc169d808d59
PoletaevEvgeny <poletaevev89@gmail.com>
2023-06-21 02:12:18 +0300

    feature/iosui-1.3 - completed 1-5 points hw

---
+++ Navigator_2.0/Navigator_2.0/ProfileViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ProfileViewController.swift
+//  Navigator_2.0
+//
+//  Created by Евгений Полетаев on 21.06.2023.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
